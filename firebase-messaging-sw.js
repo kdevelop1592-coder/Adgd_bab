f3b0e0cf89d8ff5349528e1abfd4e0c881d902d8
kalnal0928 <kalnal0928@gmail.com>
2025-12-18 10:04:00 +0900

    fix: use relative paths for service worker registration and notification icon

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -23,7 +23,7 @@ messaging.onBackgroundMessage(function (payload) {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/icons/icon-192.png'
+    icon: 'icons/icon-192.png'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
--- script.js
+++ script.js
@@ -104,7 +104,7 @@ installBtn.addEventListener('click', async () => {
 // Service Worker Registration
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/firebase-messaging-sw.js')
+        navigator.serviceWorker.register('./firebase-messaging-sw.js')
             .then(registration => {
                 console.log('Service Worker registered with scope:', registration.scope);
             })
